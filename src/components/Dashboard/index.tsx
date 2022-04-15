3f4608c80daf2c28869d2389529a5af6c5cec5e0
Bruno Vieira <b.vieira@reply.com>
2022-04-15 02:48:43 -0300

    Transactions Table

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,4 +1,5 @@
 import { Summary } from "../Symmarry";
+import { TransactionTable } from "../TransactionTable";
 import { Container } from "./style";
 
 
@@ -6,6 +7,7 @@ export function Dashboard() {
   return(
     <Container>
       <Summary></Summary>
+      <TransactionTable></TransactionTable>
     </Container>
   )
 }
